47382e8272d603a90d0a6618d54786bbf6aea4d8
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-05-02 23:17:56 +0000

    square results

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   def square
     render ({ :template => "calc_templates/square"})
   end
+
+  def square_results
+    @number = params.fetch("number").to_f
+    render ({ :template => "calc_templates/square_results"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", {:controller => "pages", :action => "square"})
+
+  get("/square/new", {:controller => "pages", :action => "square"})
+
+  get("/square/results", {:controller => "pages", :action => "square_results"})
+  
+  
 end
